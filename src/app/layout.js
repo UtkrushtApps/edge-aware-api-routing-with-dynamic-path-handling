ede94313a30313bd73d159a0d46e54d89a5e657f
Naman Muley <naman.g.muley@gmail.com>
2025-07-29 09:15:58 +0530

    Add src/app/layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ErrorBoundary from '../components/ErrorBoundary';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <ErrorBoundary>
+          {children}
+        </ErrorBoundary>
+      </body>
+    </html>
+  );
+}
